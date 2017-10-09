ee4239eab0b9f2024868c0a0d54a2f587c76d399
qq2504021719 <1104534940@qq.com>
2017-10-09 18:01:35 +0800

    完成多选日期存储,完成年月选择

--- app/src/main/java/com/bignerdranch/android/calendarmultiselect/CalendarMultiSelectActivity.java
+++ app/src/main/java/com/bignerdranch/android/calendarmultiselect/CalendarMultiSelectActivity.java
@@ -1,18 +1,24 @@
 package com.bignerdranch.android.calendarmultiselect;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 public class CalendarMultiSelectActivity extends AppCompatActivity {
 
@@ -39,6 +45,28 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
     // 日历父节点
     public LinearLayout mLinear_ri_li;
 
+    // 选择年
+    public TextView mTextView_nian;
+    // 年数据
+    public String[] stringsNian = new String[2];
+
+    // 选择月
+    public TextView mTextView_yue;
+    // 月数据
+    public String[] stringsyue = new String[]{"01","02","03","04","05","06","07","08","09","10","11","12"};
+
+    // 当天
+    public TextView mTextView_jing_ri;
+
+    // 选择器
+    private AlertDialog alertDialog1;
+
+    // 1 编辑模式 0 显示模式
+    public int mMoShi = 1;
+
+    // 已选择数据
+    HashMap<String,HashMap<String,HashMap<String,String>>> myiXuanZheData = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +79,10 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
 
         // 值操作
         value();
+
+        // 组件操作
+        // 组件操作, 操作
+        ZhuJianCaoZhuo();
     }
 
     /**
@@ -58,17 +90,83 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
      */
     public void ViewInit(){
         mLinear_ri_li = (LinearLayout)findViewById(R.id.linear_ri_li);
+        // 年
+        mTextView_nian = (TextView)findViewById(R.id.textView_nian);
+        // 月
+        mTextView_yue = (TextView)findViewById(R.id.textView_yue);
+        // 当前日
+        mTextView_jing_ri = (TextView)findViewById(R.id.textView_jing_ri);
     }
 
+    /**
+     * 组件操作
+     */
+    public void ZhuJianCaoZhuo(){
+        // 选择年
+        mTextView_nian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(mContext);
+                alertBuilder.setItems(stringsNian, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int index) {
+                        mXuanYear = stringsNian[index];
+                        // 视图创建,选择年月显示
+                        ViewShowNianYue();
+                        alertDialog1.dismiss();
+                    }
+                });
+                alertDialog1 = alertBuilder.create();
+                alertDialog1.show();
+            }
+        });
+        // 选择月
+        mTextView_yue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(mContext);
+                alertBuilder.setItems(stringsyue, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int index) {
+                        mXuanMonth = stringsyue[index];
+                        // 视图创建,选择年月显示
+                        ViewShowNianYue();
+                        alertDialog1.dismiss();
+                    }
+                });
+                alertDialog1 = alertBuilder.create();
+                alertDialog1.show();
+
+            }
+        });
+
+        // 今日
+        mTextView_jing_ri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value();
+            }
+        });
+
+    }
 
     /**
-     * 值操作
+     * 设置年 月显示
      */
-    public void value(){
-        // 当前年
-        mXuanYear = getYearMonth(1);
-        // 当前月
-        mXuanMonth = getYearMonth(2);
+    public void nianYueShow(String nianStr,String yueStr){
+        mTextView_nian.setText(nianStr+"年");
+        mTextView_yue.setText(yueStr+"月");
+    }
+
+    /**
+     * 设置默认年月
+     */
+    public void setMoRenNianYue(){
+
         // 开始日期
         mKaiSiRiQi = getFirstDayOfMonth(Integer.valueOf(mXuanYear),Integer.valueOf(mXuanMonth));
         // 结束日期
@@ -77,11 +175,40 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
         mZhouJi = getWeekOfDate(mXuanYear+"-"+mXuanMonth+"-"+mKaiSiRiQi);
         // 月末周几
         mMoZhouJi = getWeekOfDate(mXuanYear+"-"+mXuanMonth+"-"+mJieShuRiQi);
+    }
+
+    /**
+     * 值操作
+     */
+    public void value(){
+        // 当前年赋值
+        stringsNian[0] = getYearMonth(1);
+        // 下一年赋值
+        stringsNian[1] = String.valueOf(Integer.valueOf(getYearMonth(1))+1);
+        // 当前年
+        mXuanYear = getYearMonth(1);
+        // 当前月
+        mXuanMonth = getYearMonth(2);
+        // 设置默认年月
+        setMoRenNianYue();
+        // 视图创建,选择年月显示
+        ViewShowNianYue();
+    }
+
+    /**
+     * 视图创建,选择年月显示
+     */
+    public void ViewShowNianYue(){
+        // 设置年月
+        setMoRenNianYue();
+        // 显示当前年,月
+        nianYueShow(mXuanYear,mXuanMonth);
 
         // 创建布局
         CreateBuJu();
     }
 
+    // 创建日历
     public void CreateBuJu(){
         // 清空内容
         mLinear_ri_li.removeAllViews();
@@ -93,7 +220,6 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
         int arrnum = jieshu+mZhouJi+6-mMoZhouJi;
         // 构造显示数组
         String[] strings = new String[arrnum];
-        Log.i(TAG,""+mZhouJi);
         // 月初评接
         for(int i = 0;i<=mZhouJi;i++){
             strings[i] = "";
@@ -109,9 +235,12 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
         CreateView(strings);
     }
 
+    /**
+     * 创建视图
+     * @param strings
+     */
     public void CreateView(String[] strings){
         int num = 0;
-
 
         LinearLayout waiBuZuJian = CreateLinearLayout(1);
 
@@ -119,6 +248,11 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
             if(strings[i] != null){
                 LinearLayout neiBuZuJian = CreateLinearLayout(2);
                 TextView textView = CreateTextView(strings[i],0,0,0);
+                // 查看是否创建背景色
+                if(ValueIs(strings[i])){
+                    textView = CreateTextView(strings[i],1,R.color.baise,R.drawable.ri_qi_background);
+                }
+
                 neiBuZuJian.addView(textView);
                 waiBuZuJian.addView(neiBuZuJian);
                 num++;
@@ -131,11 +265,18 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
                     waiBuZuJian = CreateLinearLayout(1);
                 }
             }
-
         }
+    }
 
-
-
+    public boolean ValueIs(String string){
+        if(myiXuanZheData.get(mXuanYear) != null){
+            if(myiXuanZheData.get(mXuanYear).get(mXuanMonth) != null){
+                if(myiXuanZheData.get(mXuanYear).get(mXuanMonth).get(string) != null){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
@@ -216,17 +357,65 @@ public class CalendarMultiSelectActivity extends AppCompatActivity {
      */
     public TextView CreateTextView(String string, int leix, int color,int backgr){
         TextView textView = new TextView(mContext);
-        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(58,58);
+        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(68,68);
 
         textView.setLayoutParams(layoutParam);
         textView.setText(string);
         textView.setGravity(Gravity.CENTER);
         textView.setPadding(5,5,5,5);
+        textView.setTextColor(getResources().getColor(R.color.huise6));
 
-        if(leix == 1){
+        if(leix == 1 && string != ""){
             textView.setTextColor(getResources().getColor(color));
             textView.setBackground(getResources().getDrawable(backgr));
         }
+
+        if(mMoShi == 1){
+            final String XinShiStr = string;
+            final TextView textView1 = textView;
+            textView.setOnClickListener(new View.OnClickListener() {
+                // 是否点击 1 点击 0 未点击
+                int isDianJi = 0;
+                @Override
+                public void onClick(View view) {
+                    if(ValueIs(XinShiStr)){
+                        textView1.setTextColor(getResources().getColor(R.color.huise6));
+                        textView1.setBackground(getResources().getDrawable(R.drawable.ri_qi_background1));
+                        // 删除存储的数据
+                        myiXuanZheData.get(mXuanYear).get(mXuanMonth).remove(XinShiStr);
+                    }else{
+                        Log.i(TAG,XinShiStr);
+                        textView1.setTextColor(getResources().getColor(R.color.baise));
+                        textView1.setBackground(getResources().getDrawable(R.drawable.ri_qi_background));
+                        // 存储选择
+
+
+
+
+                        // 对应年月入存在
+
+                        if(myiXuanZheData.get(mXuanYear) != null && myiXuanZheData.get(mXuanYear).get(mXuanMonth) != null){
+                            myiXuanZheData.get(mXuanYear).get(mXuanMonth).put(XinShiStr,XinShiStr);
+                        }else{
+                            HashMap<String,String> hashMap = new HashMap<>();
+                            hashMap.put(XinShiStr,XinShiStr);
+                            HashMap<String,HashMap<String,String>> hashMap1 = new HashMap<>();
+                            hashMap1.put(mXuanMonth,hashMap);
+                            // 存储到二维数组
+                            if(myiXuanZheData.get(mXuanYear) != null){
+                                myiXuanZheData.get(mXuanYear).put(mXuanMonth,hashMap);
+                            }else{
+                                myiXuanZheData.put(mXuanYear,hashMap1);
+                            }
+
+                        }
+
+
+                    }
+                }
+            });
+        }
+
 
         return textView;
     }
